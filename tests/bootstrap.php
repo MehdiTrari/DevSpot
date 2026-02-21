f324db9eaae70f188f551677c0299ab850d41f4f
melenedufrenois <melenedufrenois@gmail.com>
2026-02-21 18:29:36 +0100

    👷 Setup tests and GitHub Actions CI

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,6 @@
 <?php
 
+use Doctrine\ORM\Tools\SchemaTool;
 use Symfony\Component\Dotenv\Dotenv;
 
 require dirname(__DIR__).'/vendor/autoload.php';
@@ -10,4 +11,25 @@ if (method_exists(Dotenv::class, 'bootEnv')) {
 
 if ($_SERVER['APP_DEBUG']) {
     umask(0000);
+}
+
+if ('test' === ($_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? null)) {
+    $kernelClass = $_SERVER['KERNEL_CLASS'] ?? $_ENV['KERNEL_CLASS'] ?? null;
+
+    if (\is_string($kernelClass) && class_exists($kernelClass)) {
+        $kernel = new $kernelClass('test', true);
+        $kernel->boot();
+
+        $entityManager = $kernel->getContainer()->get('doctrine')->getManager();
+        $metadata = $entityManager->getMetadataFactory()->getAllMetadata();
+
+        if ([] !== $metadata) {
+            $schemaTool = new SchemaTool($entityManager);
+            $schemaTool->dropSchema($metadata);
+            $schemaTool->createSchema($metadata);
+        }
+
+        $entityManager->clear();
+        $kernel->shutdown();
+    }
 }
