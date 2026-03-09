2222dfe8351ffe00ce2a3358ca3169c429bf8b0b
melenedufrenois <melenedufrenois@gmail.com>
2026-03-09 13:48:41 +0100

    :sparkles: contact form

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,17 +2,26 @@
 
 namespace App\Controller;
 
+use App\Entity\ContactMessage;
 use App\Entity\DeveloperProfile;
 use App\Entity\User;
+use App\Form\ContactMessageType;
 use App\Repository\DeveloperProfileRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class ProfileController extends AbstractController
 {
-    #[Route('/profil/{slug}', name: 'app_public_profile_show', methods: ['GET'])]
-    public function show(string $slug, DeveloperProfileRepository $developerProfileRepository): Response
+    #[Route('/profil/{slug}', name: 'app_public_profile_show', methods: ['GET', 'POST'])]
+    public function show(
+        string $slug,
+        Request $request,
+        DeveloperProfileRepository $developerProfileRepository,
+        EntityManagerInterface $entityManager,
+    ): Response
     {
         $profile = $developerProfileRepository->findOneBy(['slug' => $slug]);
 
@@ -26,6 +35,42 @@ final class ProfileController extends AbstractController
 
         if (!$profile->isPublic() && !$this->isOwner($profile)) {
             return $this->render('bundles/TwigBundle/Exception/error403.html.twig', [], new Response('', Response::HTTP_FORBIDDEN));
+        }
+
+        if (!$profile->isPublic() && $request->isMethod('POST')) {
+            throw $this->createAccessDeniedException('Impossible d envoyer un message a un profil prive.');
+        }
+
+        $contactFormView = null;
+        if ($profile->isPublic()) {
+            $contactMessage = new ContactMessage();
+
+            $currentUser = $this->getUser();
+            if ($currentUser instanceof User && null !== $currentUser->getEmail()) {
+                $contactMessage->setRecruiterEmail($currentUser->getEmail());
+            }
+
+            $contactForm = $this->createForm(ContactMessageType::class, $contactMessage);
+            $contactForm->handleRequest($request);
+
+            if ($contactForm->isSubmitted() && $contactForm->isValid()) {
+                $contactMessage->setDeveloperProfile($profile);
+                $contactMessage->setIsRead(false);
+                if (null === $contactMessage->getSubject()) {
+                    $contactMessage->setSubject('');
+                }
+
+                $entityManager->persist($contactMessage);
+                $entityManager->flush();
+
+                $this->addFlash('success', 'Votre message a bien ete envoye au developpeur.');
+
+                return $this->redirectToRoute('app_public_profile_show', [
+                    'slug' => $profile->getSlug(),
+                ]);
+            }
+
+            $contactFormView = $contactForm->createView();
         }
 
         $experiences = $profile->getExperiences()->toArray();
@@ -57,6 +102,7 @@ final class ProfileController extends AbstractController
             'education' => $education,
             'technologyNames' => array_values($technologyNames),
             'isOwner' => $this->isOwner($profile),
+            'contactForm' => $contactFormView,
         ]);
     }
 
